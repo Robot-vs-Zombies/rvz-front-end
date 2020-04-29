914ccb5591da66b19275b5d8106bc841053d12fa
Aaron <aarongillies88@gmail.com>
2020-04-28 22:01:11 -0400

    setup basic movement for test server, as well as player initalization

--- src/components/registration/Login.jsx
+++ src/components/registration/Login.jsx
@@ -1,54 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Login(props) {
-   const [user, setUsers] = useState({
-    username: '',
-    password: '',
-   })
+  const [user, setUsers] = useState({
+    username: "",
+    password: "",
+  });
 
-    const handleChange = (e) => {
+  const handleChange = (e) => {
     setUsers({
-        ...user, [e.target.name]: e.target.value
-    })
-}
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = () => {
-     axios
-     .post("https://lambda-mud-test.herokuapp.com/api/login/")
-     .then(res => {
-         console.log(res)
-        localStorage.setItem('token', res.data.key);
-        localStorage.setItem('username', user.username)
-        props.history.push('/dashboard');
-     })
-     .catch(err => {
-         console.log(err)
-     })
- }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://lambda-mud-test.herokuapp.com/api/login/", user)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.key);
+        localStorage.setItem("username", user.username);
+        props.history.push("/dashboard");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-       <form onSubmit={handleSubmit}>
-           <label htmlFor='username'>
-           <input
-                onChange={handleChange}
-                id = 'username'
-                type='text'
-                name='username'
-                value={user.username}
-                />
-           </label>
-           <label htmlFor='password'>
-               <input
-                    id='password'
-                    type='password'
-                    name='password'
-                    value={user.password}
-                />
-                    
-            </label>
-            <button>Login</button>
-       </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">
+        <input
+          onChange={handleChange}
+          id="username"
+          type="text"
+          name="username"
+          value={user.username}
+        />
+      </label>
+      <label htmlFor="password">
+        <input
+          onChange={handleChange}
+          id="password"
+          type="password"
+          name="password"
+          value={user.password}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
 }
