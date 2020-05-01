c44dc9eb442be011b9af88e0430384fba5e2c238
Aaron <aarongillies88@gmail.com>
2020-05-01 10:51:28 -0400

    fix error in utils

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -6,7 +6,7 @@ const axiosWithAuth = () => {
   return axios.create({
     baseURL: "https://agile-stream-99199.herokuapp.com/api/adv",
     headers: {
-      Authorization: token,
+      Authorization: `Token ${token}`,
     },
   });
 };
